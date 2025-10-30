483bb0e50eece8c237437869fcf08fa8d7297a70
Abhishek Pal <titwig365@gmail.com>
2025-10-30 14:25:26 +0530

    Frist_Program

--- CShopRoadMap/oneMonths/Program.cs
+++ CShopRoadMap/oneMonths/Program.cs
@@ -10,7 +10,10 @@ namespace oneMonths
     {
         static void Main(string[] args)
         {
-
+            int a = 3;
+            int b = 5;
+            Console.WriteLine(a+b);
+            Console.ReadLine();
         }
     }
 }
